21141b73c082d2346070d6284d27fbd2eb3bea76
srj101 <salim15-3117@diu.edu.bd>
2022-11-10 10:36:45 +0600

    Dashboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,49 @@ import TransportMeta from "../components/Statistics/TransportMeta";
 import BusStatistics from "../components/Statistics/BusStatistics";
 
 const Dashboard: NextPage = () => {
-	return (
-		<div className='w-100 h-100 d-flex flex-column justify-content-start '>
-			<Head>
-				<title>Smart Transport Management || DIU Transport Controll </title>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<Layout>
-				<div className='flex'>
-					<h3>Good Morning!</h3>
-				</div>
-			</Layout>
-		</div>
-	);
+  return (
+    <div className="w-100 h-100 d-flex flex-column justify-content-start ">
+      <Head>
+        <title>Smart Transport Management || DIU Transport Controll </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <div className="flex flex-col">
+          <HeadingComponent />
+          <div className="flex py-4">
+            <div className="flex flex-col w-9/12 px-4">
+              <TransportMeta
+                students={1300}
+                facalties={750}
+                sales={293811}
+                staffs={219}
+                buses={60}
+              />
+
+              <RouteStatistics
+                scheduleId="sdfkjbdf"
+                day="sunday"
+                month="november"
+                year={2019}
+                title="Students in different routes"
+              />
+
+              <BusStatistics
+                scheduleId="sdfkjbdf"
+                day="sunday"
+                month="january"
+                year={2019}
+                title="Todays Ticket Scans"
+              />
+            </div>
+            <div className="flex flex-col w-3/12 border-l-2 px-4">
+              <RightSideBar />
+            </div>
+          </div>
+        </div>
+      </Layout>
+    </div>
+  );
 };
 
 export default Dashboard;
